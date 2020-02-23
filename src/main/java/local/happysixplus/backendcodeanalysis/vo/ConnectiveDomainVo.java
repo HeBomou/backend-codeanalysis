f392829597b330ffa8c7f2cf0d61e5d92fa3b259
何泊谋 <171250634@smail.nju.edu.cn>
2020-02-23 12:38:46 +0800

    feat(vo): Modify connectiveDomainVo

--- src/main/java/local/happysixplus/backendcodeanalysis/vo/ConnectiveDomainVo.java
+++ src/main/java/local/happysixplus/backendcodeanalysis/vo/ConnectiveDomainVo.java
@@ -1,9 +1,12 @@
 package local.happysixplus.backendcodeanalysis.vo;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class ConnectiveDomainVo {
 
     int vertexNum;
+    List<EdgeVo> edgeVos;
 }
