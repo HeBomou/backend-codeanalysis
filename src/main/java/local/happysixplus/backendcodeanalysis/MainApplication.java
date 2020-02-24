b2e45049094951bbbd6550096266df17cccb92f4
forsakenspirit <2369351080@qq.com>
2020-02-24 17:23:02 +0800

    feat(data layer) 添加了对jpa的依赖，添加了po层，data层和dataimpl层(data层用于提供访问数据库的接口),dataimpl层用于实现这些接口）

--- src/main/java/local/happysixplus/backendcodeanalysis/MainApplication.java
+++ src/main/java/local/happysixplus/backendcodeanalysis/MainApplication.java
@@ -3,6 +3,7 @@ package local.happysixplus.backendcodeanalysis;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import local.happysixplus.backendcodeanalysis.cli.CLI;
@@ -12,7 +13,7 @@ import lombok.var;
 public class MainApplication {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(MainApplication.class, args);
+		//SpringApplication.run(MainApplication.class, args);
 		var cli = new CLI();
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		while(true) {
--- src/main/java/local/happysixplus/backendcodeanalysis/controller/DemoController.java
+++ src/main/java/local/happysixplus/backendcodeanalysis/controller/DemoController.java
@@ -1,16 +1,16 @@
 package local.happysixplus.backendcodeanalysis.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
 public class DemoController {
-
     @GetMapping(value="/")
     public String getMethodName() {
         return "Hello world";
     }
-    
 }
