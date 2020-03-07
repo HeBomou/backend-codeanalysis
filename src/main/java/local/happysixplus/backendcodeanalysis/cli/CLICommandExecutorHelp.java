979628ab859b23026172705988be5263bb54796b
何泊谋 <171250634@smail.nju.edu.cn>
2020-03-07 23:12:03 +0800

    feat(cli): Improve help and remove domain num

--- src/main/java/local/happysixplus/backendcodeanalysis/cli/CLICommandExecutorHelp.java
+++ src/main/java/local/happysixplus/backendcodeanalysis/cli/CLICommandExecutorHelp.java
@@ -9,6 +9,13 @@ public class CLICommandExecutorHelp implements CLICommandExecutor {
     @Override
     public void execute(String[] params, Scanner scanner, GraphService graphService) {
         // TODO: 需要完成help命令
-        System.out.println("TODO");
+        System.out.println("init [PATH]");
+        System.out.println("basic-attribute");
+        System.out.println("connective-domain");
+        System.out.println("connective-domain-num");
+        System.out.println("set-closeness-min [VALUE]");
+        System.out.println("connective-domain-with-closeness-min");
+        System.out.println("shortest-path");
+        System.out.println("quit");
     }
 }
