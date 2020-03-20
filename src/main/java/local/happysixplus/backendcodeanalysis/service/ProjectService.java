8ebbf406292b412817b123847d6464624e77eb08
何泊谋 <171250634@smail.nju.edu.cn>
2020-03-20 21:34:54 +0800

    fix(service): Add projectName param for addProject

--- src/main/java/local/happysixplus/backendcodeanalysis/service/ProjectService.java
+++ src/main/java/local/happysixplus/backendcodeanalysis/service/ProjectService.java
@@ -16,7 +16,7 @@ public interface ProjectService {
      * @param path 项目代码的git仓库url
      * @return 返回新项目的id
      */
-    ProjectAllVo addProject(String url);
+    ProjectAllVo addProject(String projectName, String url);
 
     void removeProject(Long id);
 
