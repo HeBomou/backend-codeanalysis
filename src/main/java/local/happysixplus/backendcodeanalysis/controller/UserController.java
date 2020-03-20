e420f6902c0dbfe734bbeeb34655e87796260e02
何泊谋 <171250634@smail.nju.edu.cn>
2020-03-20 23:09:58 +0800

    fix(controller): Fix

--- src/main/java/local/happysixplus/backendcodeanalysis/controller/UserController.java
+++ src/main/java/local/happysixplus/backendcodeanalysis/controller/UserController.java
@@ -1,25 +1,27 @@
 package local.happysixplus.backendcodeanalysis.controller;
 
-
 import org.springframework.web.bind.annotation.RestController;
 
 import local.happysixplus.backendcodeanalysis.service.UserService;
 import local.happysixplus.backendcodeanalysis.vo.UserVo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 
-
-
-@RestController(value = "/user")
+@RestController
+@RequestMapping(value = "/user")
 public class UserController {
     @Autowired
     UserService service;
-    
+
     @PostMapping
     public void postUser(@RequestBody UserVo vo) {
         service.addUser(vo);
@@ -30,8 +32,13 @@ public class UserController {
         service.removeUser(id);
     }
 
-    @PutMapping(value="/{id}")
+    @PutMapping(value = "/{id}")
     public void putUser(@PathVariable Long id, @RequestBody UserVo vo) {
         service.updateUser(vo);
+    }
+
+    @GetMapping
+    public List<UserVo> getAllUsers() {
+        return service.getAllUsers();
     }
 }
