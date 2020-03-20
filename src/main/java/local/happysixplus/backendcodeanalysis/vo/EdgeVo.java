07cd10588d1ad36fae29743e61d0b5a5087d52d7
胡丹丹 <171250034@smail.nju.edu.cn>
2020-03-20 17:15:38 +0800

    style(vo):添加注解

--- src/main/java/local/happysixplus/backendcodeanalysis/vo/EdgeVo.java
+++ src/main/java/local/happysixplus/backendcodeanalysis/vo/EdgeVo.java
@@ -1,8 +1,10 @@
 package local.happysixplus.backendcodeanalysis.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class EdgeVo {
 
     Long id;
--- src/main/java/local/happysixplus/backendcodeanalysis/vo/SubgraphVo.java
+++ src/main/java/local/happysixplus/backendcodeanalysis/vo/SubgraphVo.java
@@ -2,12 +2,14 @@ package local.happysixplus.backendcodeanalysis.vo;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
  * 设置了阈值的子图
  */
 @Data
+@AllArgsConstructor
 public class SubgraphVo {
 
     Long id;
