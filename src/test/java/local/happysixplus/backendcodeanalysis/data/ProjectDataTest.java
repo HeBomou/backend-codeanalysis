255c68513d5cd98cafa204f356fcdf489fee4df5
何泊谋 <171250634@smail.nju.edu.cn>
2020-03-27 19:27:40 +0800

    test(data): Enrich project data test

--- src/test/java/local/happysixplus/backendcodeanalysis/data/ProjectDataTest.java
+++ src/test/java/local/happysixplus/backendcodeanalysis/data/ProjectDataTest.java
@@ -72,28 +72,49 @@ class ProjectDataTest {
     public void insertWholeData() {
         var projectPo = new ProjectPo(null, 1L, "projectName 1", null, null, null);
         // Vertices
-        var v1 = new VertexPo(null, "cnmd1", "nmbd yuanma 1", "anotation", -1f, -1f);
-        var v2 = new VertexPo(null, "cnmd2", "nmbd yuanma 2", "anotation", -1f, -1f);
-        var v3 = new VertexPo(null, "cnmd3", "nmbd yuanma 3", "anotation", -1f, -1f);
+        var v1 = new VertexPo(null, "cnmd1", "nmbd yuanma 1", "anotation", 0f, 0f);
+        var v2 = new VertexPo(null, "cnmd2", "nmbd yuanma 2", "anotation", 0f, 0f);
+        var v3 = new VertexPo(null, "cnmd3", "nmbd yuanma 3", "anotation", 0f, 0f);
+        var v4 = new VertexPo(null, "cnmd4", "nmbd yuanma 4", "anotation", 0f, 0f);
         var vertices = new HashSet<VertexPo>();
         vertices.add(v1);
         vertices.add(v2);
         vertices.add(v3);
+        vertices.add(v4);
         projectPo.setVertices(vertices);
         // Edges
-        var e1 = new EdgePo(null, v1, v2, 0.1d, "edge 1");
-        var e2 = new EdgePo(null, v2, v3, 0.2d, "edge 2");
+        var e1 = new EdgePo(null, v1, v2, 0.3d, "edge 1");
+        var e2 = new EdgePo(null, v2, v3, 0.1d, "edge 2");
+        var e3 = new EdgePo(null, v3, v4, 0.3d, "edge 3");
         var edges = new HashSet<EdgePo>();
         edges.add(e1);
         edges.add(e2);
+        edges.add(e3);
         projectPo.setEdges(edges);
         // Subgraphs
-        var connectiveDomain1 = new ConnectiveDomainPo(null, vertices, edges, "anotation");
+        var connectiveDomain1 = new ConnectiveDomainPo(null, vertices, edges, "anotation cd1");
+        var cd2vSet = new HashSet<VertexPo>();
+        cd2vSet.add(v1);
+        cd2vSet.add(v2);
+        var cd2eSet = new HashSet<EdgePo>();
+        cd2eSet.add(e1);
+        var connectiveDomain2 = new ConnectiveDomainPo(null, cd2vSet, cd2eSet, "anotation cd2");
+        var cd3vSet = new HashSet<VertexPo>();
+        cd3vSet.add(v3);
+        cd3vSet.add(v4);
+        var cd3eSet = new HashSet<EdgePo>();
+        cd3eSet.add(e3);
+        var connectiveDomain3 = new ConnectiveDomainPo(null, cd3vSet, cd3eSet, "anotation cd3");
         var connectiveDomains1 = new HashSet<ConnectiveDomainPo>();
         connectiveDomains1.add(connectiveDomain1);
-        var subgraph = new SubgraphPo(null, 0d, "default subgraph", connectiveDomains1);
+        var connectiveDomains2 = new HashSet<ConnectiveDomainPo>();
+        connectiveDomains2.add(connectiveDomain2);
+        connectiveDomains2.add(connectiveDomain3);
+        var defaultSubgraph = new SubgraphPo(null, 0d, "default subgraph", connectiveDomains1);
+        var subgraph1 = new SubgraphPo(null, 0.2d, "Subgraph 1 cnmd", connectiveDomains2);
         var subgraphs = new HashSet<SubgraphPo>();
-        subgraphs.add(subgraph);
+        subgraphs.add(defaultSubgraph);
+        subgraphs.add(subgraph1);
         projectPo.setSubgraphs(subgraphs);
         data.save(projectPo);
     }
