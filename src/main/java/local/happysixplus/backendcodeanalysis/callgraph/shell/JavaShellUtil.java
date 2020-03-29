92c15b1949e0a4d34612f21e51a59be13425a777
何泊谋 <171250634@smail.nju.edu.cn>
2020-03-29 22:54:49 +0800

    fix(callgraph): Recover clone.sh

--- src/main/java/local/happysixplus/backendcodeanalysis/callgraph/shell/JavaShellUtil.java
+++ src/main/java/local/happysixplus/backendcodeanalysis/callgraph/shell/JavaShellUtil.java
@@ -28,7 +28,7 @@ public class JavaShellUtil {
     public static void ExecClone(String projectName,String url){
         try{
 
-            String command="mkdir temp/"+projectName+" ; git clone "+url+" temp/"+projectName+" ; cd temp/"+projectName+" ;"+"mvn package";
+            String command="mkdir temp/"+projectName+" ; git clone "+url+" temp/"+projectName+" ; cd temp/"+projectName+" ;"+"/Users/hebo/Workspace/Homework/se3/backend-codeanalysis/mvnw package";
             String[] commands=new String[]{"/bin/sh","-c",command};
             Process process=Runtime.getRuntime().exec(commands,null,null);
             ExecOutput(process);
--- src/main/resources/Scripts/clone.sh
+++ src/main/resources/Scripts/clone.sh
@@ -1,4 +1,4 @@
 mkdir src/main/resources/temp/$1
 git clone $2 src/main/resources/temp/$1
 cd src/main/resources/temp/$1
-/Users/hebo/Workspace/Homework/se3/backend-codeanalysis/mvnw package
+mvn package
