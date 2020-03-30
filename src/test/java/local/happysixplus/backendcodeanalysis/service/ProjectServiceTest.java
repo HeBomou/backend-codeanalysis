7f508e884076753a9d83b35ef344bb7769792b76
何泊谋 <171250634@smail.nju.edu.cn>
2020-03-30 10:42:07 +0800

    refactor: use arrays.asList for test

--- src/test/java/local/happysixplus/backendcodeanalysis/service/ProjectServiceTest.java
+++ src/test/java/local/happysixplus/backendcodeanalysis/service/ProjectServiceTest.java
@@ -2,7 +2,7 @@ package local.happysixplus.backendcodeanalysis.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
@@ -25,11 +25,9 @@ import local.happysixplus.backendcodeanalysis.vo.ConnectiveDomainDynamicVo;
 import local.happysixplus.backendcodeanalysis.vo.ConnectiveDomainStaticVo;
 import local.happysixplus.backendcodeanalysis.vo.EdgeDynamicVo;
 import local.happysixplus.backendcodeanalysis.vo.EdgeStaticVo;
-import local.happysixplus.backendcodeanalysis.vo.PathVo;
 import local.happysixplus.backendcodeanalysis.vo.ProjectAllVo;
 import local.happysixplus.backendcodeanalysis.vo.ProjectDynamicVo;
 import local.happysixplus.backendcodeanalysis.vo.ProjectStaticVo;
-import local.happysixplus.backendcodeanalysis.vo.SubgraphAllVo;
 import local.happysixplus.backendcodeanalysis.vo.SubgraphDynamicVo;
 import local.happysixplus.backendcodeanalysis.vo.SubgraphStaticVo;
 import local.happysixplus.backendcodeanalysis.vo.VertexDynamicVo;
@@ -65,25 +63,19 @@ public class ProjectServiceTest {
         // Vertices
         var v1 = new VertexPo(3L, "v1", "dian1", "a1", 0f, 0f);
         var v2 = new VertexPo(4L, "v2", "dian2", "a2", 0f, 0f);
-        var vertices = new HashSet<VertexPo>();
-        vertices.add(v1);
-        vertices.add(v2);
+        var vertices = new HashSet<VertexPo>(Arrays.asList(v1, v2));
         projectPo.setVertices(vertices);
         // Edges
         var e1 = new EdgePo(3L, v1, v2, 0.3d, "bian1");
-        var edges = new HashSet<EdgePo>();
-        edges.add(e1);
+        var edges = new HashSet<EdgePo>(Arrays.asList(e1));
         projectPo.setEdges(edges);
         // Subgraphs
         var connectiveDomain1 = new ConnectiveDomainPo(2L, vertices, edges, "acd1", "#CDBE70");
-        var connectiveDomains1 = new HashSet<ConnectiveDomainPo>();
-        connectiveDomains1.add(connectiveDomain1);
+        var connectiveDomains1 = new HashSet<ConnectiveDomainPo>(Arrays.asList(connectiveDomain1));
         var defaultSubgraph = new SubgraphPo(4L, 0d, "default subgraph", connectiveDomains1);
-        var subgraphs = new HashSet<SubgraphPo>();
-        subgraphs.add(defaultSubgraph);
+        var subgraphs = new HashSet<SubgraphPo>(Arrays.asList(defaultSubgraph));
         projectPo.setSubgraphs(subgraphs);
-        List<ProjectPo> dataRes = new ArrayList<>();
-        dataRes.add(projectPo);
+        List<ProjectPo> dataRes = Arrays.asList(projectPo);
 
         // 打桩
         Mockito.when(projectData.findByUserId(5L)).thenReturn(dataRes);
@@ -94,37 +86,24 @@ public class ProjectServiceTest {
         // 验证数据生成
         var vs1 = new VertexStaticVo(3L, "v1", "dian1");
         var vs2 = new VertexStaticVo(4L, "v2", "dian2");
-        var vss = new ArrayList<VertexStaticVo>();
-        vss.add(vs1);
-        vss.add(vs2);
+        var vss = Arrays.asList(vs1, vs2);
         var vd1 = new VertexDynamicVo(3L, "a1", 0f, 0f);
         var vd2 = new VertexDynamicVo(4L, "a2", 0f, 0f);
-        var vds = new ArrayList<VertexDynamicVo>();
-        vds.add(vd1);
-        vds.add(vd2);
+        var vds = Arrays.asList(vd1, vd2);
         var es1 = new EdgeStaticVo(3L, 3L, 4L, 0.3d);
-        var ess = new ArrayList<EdgeStaticVo>();
-        ess.add(es1);
+        var ess = Arrays.asList(es1);
         var ed1 = new EdgeDynamicVo(3L, "bian1");
-        var eds = new ArrayList<EdgeDynamicVo>();
-        eds.add(ed1);
-        var cs1vids = new ArrayList<Long>();
-        cs1vids.add(3L);
-        cs1vids.add(4L);
-        var cs1eids = new ArrayList<Long>();
-        cs1eids.add(3L);
+        var eds = Arrays.asList(ed1);
+        var cs1vids = Arrays.asList(3L, 4L);
+        var cs1eids = Arrays.asList(3L);
         var cs1 = new ConnectiveDomainStaticVo(2L, cs1vids, cs1eids);
-        var css = new ArrayList<ConnectiveDomainStaticVo>();
-        css.add(cs1);
+        var css = Arrays.asList(cs1);
         var cd1 = new ConnectiveDomainDynamicVo(2L, "acd1", "#CDBE70");
-        var cds = new ArrayList<ConnectiveDomainDynamicVo>();
-        cds.add(cd1);
+        var cds = Arrays.asList(cd1);
         var subgs1 = new SubgraphStaticVo(4L, 0d, css);
-        var subgss = new ArrayList<SubgraphStaticVo>();
-        subgss.add(subgs1);
+        var subgss = Arrays.asList(subgs1);
         var subgd1 = new SubgraphDynamicVo(4L, "default subgraph", cds);
-        var subgds = new ArrayList<SubgraphDynamicVo>();
-        subgds.add(subgd1);
+        var subgds = Arrays.asList(subgd1);
         var projectDynamicVo = new ProjectDynamicVo(2L, "projC", vds, eds, subgds);
         var projectStaticVo = new ProjectStaticVo(2L, vss, ess, subgss);
         var projectAllVo = new ProjectAllVo(2L, projectStaticVo, projectDynamicVo);
--- src/test/java/local/happysixplus/backendcodeanalysis/service/UserServiceTest.java
+++ src/test/java/local/happysixplus/backendcodeanalysis/service/UserServiceTest.java
@@ -1,20 +1,31 @@
 package local.happysixplus.backendcodeanalysis.service;
 
+import local.happysixplus.backendcodeanalysis.data.UserData;
+import local.happysixplus.backendcodeanalysis.po.UserPo;
 import local.happysixplus.backendcodeanalysis.vo.UserVo;
+import lombok.var;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class UserServiceTest {
 
-    UserVo vo;
+    @MockBean
+    UserData data;
 
     @Autowired
     UserService service;
@@ -30,21 +41,52 @@ class UserServiceTest {
 
     @Test
     void addUser() {
+        // 执行
+        var userVo = new UserVo(null, "tester1", "xxxx");
+        service.addUser(userVo);
 
-        service.addUser(vo);
-
+        // 验证
+        var userPo = new UserPo(null, "tester1", "xxxx");
+        Mockito.verify(data).save(userPo);
     }
 
     @Test
     void removeUser() {
+        var id = 12L;
+        service.removeUser(id);
+
+        Mockito.verify(data).deleteById(id);
     }
 
     @Test
     void updateUser() {
+        // 执行
+        var userVo = new UserVo(2L, "tester3", "hello");
+        service.updateUser(userVo);
+
+        // 验证
+        var userPo = new UserPo(2L, "tester3", "hello");
+        Mockito.verify(data).save(userPo);
     }
 
     @Test
     void getAllUsers() {
+        // 打桩
+        var up1 = new UserPo(2L, "tester3", "hello");
+        var up2 = new UserPo(4L, "tester5", "hellooo");
+        var up3 = new UserPo(8L, "zyq", "nb!");
+        var ups = Arrays.asList(up3, up2, up1);
+        Mockito.when(data.findAll()).thenReturn(ups);
+
+        // 执行
+        var res = service.getAllUsers();
+
+        // 验证
+        var uv1 = new UserVo(2L, "tester3", null);
+        var uv2 = new UserVo(4L, "tester5", null);
+        var uv3 = new UserVo(8L, "zyq", null);
+        var uvs = Arrays.asList(uv1, uv3, uv2);
+        assertEquals(res, uvs);
     }
 
     @Test
