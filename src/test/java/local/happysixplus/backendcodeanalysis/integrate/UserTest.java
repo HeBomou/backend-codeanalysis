00df36996e2909dcf7b3e66a59b5197f139e8433
何泊谋 <171250634@smail.nju.edu.cn>
2020-03-30 12:47:08 +0800

    test: Add user intergrated test

--- src/test/java/local/happysixplus/backendcodeanalysis/integrate/UserTest.java
+++ src/test/java/local/happysixplus/backendcodeanalysis/integrate/UserTest.java
@@ -82,4 +82,66 @@ public class UserTest {
                 .content(JSONObject.toJSONString(sessionVo)))
                 .andExpect(MockMvcResultMatchers.status().is5xxServerError()).andReturn();
     }
+
+    @Test
+    public void updateUser1() throws Exception {
+        data.deleteAll();
+        // 注册
+        var userVo = new UserVo(null, "hello", "XXX");
+        mockMvc.perform(MockMvcRequestBuilders.post("/user").contentType(MediaType.APPLICATION_JSON)
+                .content(JSONObject.toJSONString(userVo))).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+        // 登陆
+        var sessionVo = new SessionVo("hello", "XXX");
+        var res = mockMvc
+                .perform(MockMvcRequestBuilders.post("/session").contentType(MediaType.APPLICATION_JSON)
+                        .content(JSONObject.toJSONString(sessionVo)))
+                .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+        Long userId = Long.valueOf(res.getResponse().getContentAsString());
+        // 更新用户密码
+        userVo.setId(userId);
+        userVo.setPwdMd5("AAA");
+        mockMvc.perform(MockMvcRequestBuilders.put("/user/{id}", userId).contentType(MediaType.APPLICATION_JSON)
+                .content(JSONObject.toJSONString(userVo))).andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+        // 原密码无法登陆
+        mockMvc.perform(MockMvcRequestBuilders.post("/session").contentType(MediaType.APPLICATION_JSON)
+                .content(JSONObject.toJSONString(sessionVo)))
+                .andExpect(MockMvcResultMatchers.status().is5xxServerError()).andReturn();
+        // 新密码成功登陆
+        sessionVo.setPwdMd5("AAA");
+        mockMvc.perform(MockMvcRequestBuilders.post("/session").contentType(MediaType.APPLICATION_JSON)
+                .content(JSONObject.toJSONString(sessionVo))).andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    public void updateUser2() throws Exception {
+        data.deleteAll();
+        // 注册
+        var userVo = new UserVo(null, "hello", "XXX");
+        mockMvc.perform(MockMvcRequestBuilders.post("/user").contentType(MediaType.APPLICATION_JSON)
+                .content(JSONObject.toJSONString(userVo))).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+        // 登陆
+        var sessionVo = new SessionVo("hello", "XXX");
+        var res = mockMvc
+                .perform(MockMvcRequestBuilders.post("/session").contentType(MediaType.APPLICATION_JSON)
+                        .content(JSONObject.toJSONString(sessionVo)))
+                .andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+        Long userId = Long.valueOf(res.getResponse().getContentAsString());
+        // 更新用户名
+        userVo.setId(userId);
+        userVo.setUsername("hhhh");
+        mockMvc.perform(MockMvcRequestBuilders.put("/user/{id}", userId).contentType(MediaType.APPLICATION_JSON)
+                .content(JSONObject.toJSONString(userVo))).andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+        // 原用户名无法登陆
+        mockMvc.perform(MockMvcRequestBuilders.post("/session").contentType(MediaType.APPLICATION_JSON)
+                .content(JSONObject.toJSONString(sessionVo)))
+                .andExpect(MockMvcResultMatchers.status().is5xxServerError()).andReturn();
+        // 新用户名成功登陆
+        sessionVo.setUsername("hhhh");
+        mockMvc.perform(MockMvcRequestBuilders.post("/session").contentType(MediaType.APPLICATION_JSON)
+                .content(JSONObject.toJSONString(sessionVo))).andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+    }
 }
