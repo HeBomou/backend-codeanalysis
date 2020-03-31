508283b878e80059465ba7205001cabb86e5cd9e
何泊谋 <171250634@smail.nju.edu.cn>
2020-04-01 01:34:56 +0800

    fix(po): change source code to mediatext

--- src/main/java/local/happysixplus/backendcodeanalysis/po/VertexPo.java
+++ src/main/java/local/happysixplus/backendcodeanalysis/po/VertexPo.java
@@ -1,5 +1,6 @@
 package local.happysixplus.backendcodeanalysis.po;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,6 +22,7 @@ public class VertexPo {
 
     String functionName;
 
+    @Column(columnDefinition = "MEDIUMTEXT")
     String sourceCode;
 
     String anotation;
