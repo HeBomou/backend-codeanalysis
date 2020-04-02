3c67ed4e62d37e4ffc28ccb76a4a7d68386fa562
forsakenspirit <2369351080@qq.com>
2020-04-02 22:05:24 +0800

    test(data):新增

---
+++ src/test/java/local/happysixplus/backendcodeanalysis/data/ConnectiveDomainColorDynamicDataTest.java
@@ -0,0 +1,105 @@
+package local.happysixplus.backendcodeanalysis.data;
+
+import local.happysixplus.backendcodeanalysis.po.ConnectiveDomainColorDynamicPo;
+import lombok.var;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.*;
+
+@SpringBootTest
+class ConnectiveDomainColorDynamicDataTest {
+
+    @Autowired
+    ConnectiveDomainColorDynamicData data;
+
+    ConnectiveDomainColorDynamicPo connectiveDomainColorDynamicPo = new ConnectiveDomainColorDynamicPo();
+
+    @BeforeEach
+    void init() {
+        data.deleteAll();
+        connectiveDomainColorDynamicPo = new ConnectiveDomainColorDynamicPo(1234L, 1L, "SKTFaker");
+    }
+
+    @AfterEach
+    void tearDown() {
+        data.deleteAll();
+    }
+
+    @Test
+    public void testInsert() {
+        connectiveDomainColorDynamicPo = data.save(connectiveDomainColorDynamicPo);
+        ConnectiveDomainColorDynamicPo resPo = data.findById(connectiveDomainColorDynamicPo.getId()).get();
+        assertEquals(resPo.getId(), connectiveDomainColorDynamicPo.getId());
+        assertEquals(resPo.getProjectId(), connectiveDomainColorDynamicPo.getProjectId());
+        assertEquals(resPo.getColor(), connectiveDomainColorDynamicPo.getColor());
+    }
+
+    @Test
+    public void testUpdate() {
+        connectiveDomainColorDynamicPo = data.save(connectiveDomainColorDynamicPo);
+        connectiveDomainColorDynamicPo = new ConnectiveDomainColorDynamicPo(connectiveDomainColorDynamicPo.getId(), 4396L,
+                "Mother fucker");
+        connectiveDomainColorDynamicPo = data.save(connectiveDomainColorDynamicPo);
+        ConnectiveDomainColorDynamicPo resPo = data.findById(connectiveDomainColorDynamicPo.getId()).get();
+        assertEquals(resPo.getId(), connectiveDomainColorDynamicPo.getId());
+        assertEquals(resPo.getProjectId(), connectiveDomainColorDynamicPo.getProjectId());
+        assertEquals(resPo.getColor(), connectiveDomainColorDynamicPo.getColor());
+    }
+
+    @Test
+    public void testRemove() {
+        connectiveDomainColorDynamicPo = data.save(connectiveDomainColorDynamicPo);
+        data.deleteById(connectiveDomainColorDynamicPo.getId());
+        var res = data.findById(connectiveDomainColorDynamicPo.getId()).orElse(null);
+        assertEquals(res, null);
+    }
+
+    @Test
+    public void testFindByProjectId() {
+        connectiveDomainColorDynamicPo = data.save(connectiveDomainColorDynamicPo);
+        ConnectiveDomainColorDynamicPo connectiveDomainColorDynamicPo1 = data
+                .save(new ConnectiveDomainColorDynamicPo(12345L, 1L, "SKTKhan"));
+        ConnectiveDomainColorDynamicPo connectiveDomainColorDynamicPo2 = data
+                .save(new ConnectiveDomainColorDynamicPo(12346L, 1L, "SKTCLid"));
+        List<ConnectiveDomainColorDynamicPo> list = data.findByProjectId(1L);
+        list.sort((a, b) -> (int) (a.getId() - b.getId()));
+        assertEquals(list.get(0).getId(), connectiveDomainColorDynamicPo.getId());
+        assertEquals(list.get(0).getProjectId(), connectiveDomainColorDynamicPo.getProjectId());
+        assertEquals(list.get(0).getColor(), connectiveDomainColorDynamicPo.getColor());
+
+        assertEquals(list.get(1).getId(), connectiveDomainColorDynamicPo1.getId());
+        assertEquals(list.get(1).getProjectId(), connectiveDomainColorDynamicPo1.getProjectId());
+        assertEquals(list.get(1).getColor(), connectiveDomainColorDynamicPo1.getColor());
+
+        assertEquals(list.get(2).getId(), connectiveDomainColorDynamicPo2.getId());
+        assertEquals(list.get(2).getProjectId(), connectiveDomainColorDynamicPo2.getProjectId());
+        assertEquals(list.get(2).getColor(), connectiveDomainColorDynamicPo2.getColor());
+    }
+
+    @Test
+    public void testDeleteByProjectId() {
+        connectiveDomainColorDynamicPo = data.save(connectiveDomainColorDynamicPo);
+        data.save(new ConnectiveDomainColorDynamicPo(12347L, 1L, "SKTKhan"));
+        data.save(new ConnectiveDomainColorDynamicPo(12348L, 1L, "SKTCLid"));
+        data.deleteByProjectId(connectiveDomainColorDynamicPo.getId());
+        List<ConnectiveDomainColorDynamicPo> list = data.findByProjectId(connectiveDomainColorDynamicPo.getId());
+        assertEquals(list, new ArrayList<ConnectiveDomainColorDynamicPo>());
+    }
+
+    @Test
+    public void testCountByProjectId() {
+        connectiveDomainColorDynamicPo = data.save(connectiveDomainColorDynamicPo);
+        data.save(new ConnectiveDomainColorDynamicPo(12347L, 1L, "SKTKhan"));
+        data.save(new ConnectiveDomainColorDynamicPo(12348L, 1L, "SKTCLid"));
+        data.deleteByProjectId(connectiveDomainColorDynamicPo.getId());
+        List<ConnectiveDomainColorDynamicPo> list = data.findByProjectId(connectiveDomainColorDynamicPo.getId());
+        assertEquals(list, new ArrayList<ConnectiveDomainColorDynamicPo>());
+    }
+}
---
+++ src/test/java/local/happysixplus/backendcodeanalysis/data/VertexPositionDynamicTest.java
@@ -0,0 +1,105 @@
+package local.happysixplus.backendcodeanalysis.data;
+
+import local.happysixplus.backendcodeanalysis.po.VertexPositionDynamicPo;
+import lombok.var;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.*;
+
+@SpringBootTest
+class VertexPositionDynamicDataTest {
+
+    @Autowired
+    VertexPositionDynamicData data;
+
+    VertexPositionDynamicPo vertexPositionDynamicPo = new VertexPositionDynamicPo();
+
+    @BeforeEach
+    void init() {
+        data.deleteAll();
+        vertexPositionDynamicPo = new VertexPositionDynamicPo(4444L, 1L, 0.1f,0.1f);
+    }
+
+    @AfterEach
+    void tearDown() {
+        data.deleteAll();
+    }
+
+    @Test
+    public void testInsert() {
+        vertexPositionDynamicPo = data.save(vertexPositionDynamicPo);
+        VertexPositionDynamicPo resPo = data.findById(vertexPositionDynamicPo.getId()).get();
+        assertEquals(resPo.getId(), vertexPositionDynamicPo.getId());
+        assertEquals(resPo.getProjectId(), vertexPositionDynamicPo.getProjectId());
+        assertEquals(resPo.getX(), vertexPositionDynamicPo.getX());
+        assertEquals(resPo.getY(), vertexPositionDynamicPo.getY());
+    }
+
+    @Test
+    public void testUpdate() {
+        vertexPositionDynamicPo = data.save(vertexPositionDynamicPo);
+        vertexPositionDynamicPo = new VertexPositionDynamicPo(vertexPositionDynamicPo.getId(), 4396L,0.2f,0.2f);
+        vertexPositionDynamicPo = data.save(vertexPositionDynamicPo);
+        VertexPositionDynamicPo resPo = data.findById(vertexPositionDynamicPo.getId()).get();
+        assertEquals(resPo.getId(), vertexPositionDynamicPo.getId());
+        assertEquals(resPo.getProjectId(), vertexPositionDynamicPo.getProjectId());
+        assertEquals(resPo.getX(), vertexPositionDynamicPo.getX());
+        assertEquals(resPo.getY(), vertexPositionDynamicPo.getY());
+
+    }
+
+    @Test
+    public void testRemove() {
+        vertexPositionDynamicPo = data.save(vertexPositionDynamicPo);
+        data.deleteById(vertexPositionDynamicPo.getId());
+        var res = data.findById(vertexPositionDynamicPo.getId()).orElse(null);
+        assertEquals(res, null);
+    }
+
+    @Test
+    public void testFindByProjectId() {
+        vertexPositionDynamicPo = data.save(vertexPositionDynamicPo);
+        VertexPositionDynamicPo vertexPositionDynamicPo1 = data.save(new VertexPositionDynamicPo(4657L, 1L, 0.3f,0.3f));
+        VertexPositionDynamicPo vertexPositionDynamicPo2 = data.save(new VertexPositionDynamicPo(4658L, 1L, 0.4f,0.4f));
+        List<VertexPositionDynamicPo> list = data.findByProjectId(1L);
+        list.sort((a, b) -> (int) (a.getId() - b.getId()));
+        assertEquals(list.get(0).getId(), vertexPositionDynamicPo.getId());
+        assertEquals(list.get(0).getProjectId(), vertexPositionDynamicPo.getProjectId());
+        assertEquals(list.get(0).getX(), vertexPositionDynamicPo.getX());
+        assertEquals(list.get(0).getY(), vertexPositionDynamicPo.getY());
+
+        assertEquals(list.get(1).getId(), vertexPositionDynamicPo1.getId());
+        assertEquals(list.get(1).getProjectId(), vertexPositionDynamicPo1.getProjectId());
+        assertEquals(list.get(1).getX(), vertexPositionDynamicPo1.getX());
+        assertEquals(list.get(0).getY(), vertexPositionDynamicPo1.getY());
+
+        assertEquals(list.get(2).getId(), vertexPositionDynamicPo2.getId());
+        assertEquals(list.get(2).getProjectId(), vertexPositionDynamicPo2.getProjectId());
+        assertEquals(list.get(2).getX(), vertexPositionDynamicPo2.getX());
+        assertEquals(list.get(0).getY(), vertexPositionDynamicPo2.getY());
+    }
+
+    @Test
+    public void testDeleteByProjectId() {
+        vertexPositionDynamicPo = data.save(vertexPositionDynamicPo);
+        data.save(new VertexPositionDynamicPo(4657L, 1L, 0.5f,0.5f));
+        data.save(new VertexPositionDynamicPo(4658L, 1L, 0.7f,0.7f));
+        data.deleteByProjectId(vertexPositionDynamicPo.getId());
+        List<VertexPositionDynamicPo> list = data.findByProjectId(vertexPositionDynamicPo.getId());
+        assertEquals(list, new ArrayList<VertexPositionDynamicPo>());
+    }
+    public void testCountByProjectId(){
+        vertexPositionDynamicPo = data.save(vertexPositionDynamicPo);
+        data.save(new VertexPositionDynamicPo(4657L, 1L, 0.5f,0.5f));
+        data.save(new VertexPositionDynamicPo(4658L, 1L, 0.7f,0.7f));
+        int count=data.countByProjectId(1L);
+        assertEquals(1,count);
+    }
+}
