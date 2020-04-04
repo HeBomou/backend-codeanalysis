4286909b1dd0166a3bea7538587e9e8ba738659d
胡丹丹 <171250034@smail.nju.edu.cn>
2020-04-05 02:32:42 +0800

    style(impl):去掉无用头文件

--- src/main/java/local/happysixplus/backendcodeanalysis/service/impl/AdminSessionServiceImpl.java
+++ src/main/java/local/happysixplus/backendcodeanalysis/service/impl/AdminSessionServiceImpl.java
@@ -1,8 +1,5 @@
 package local.happysixplus.backendcodeanalysis.service.impl;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import local.happysixplus.backendcodeanalysis.data.AdminUserData;
