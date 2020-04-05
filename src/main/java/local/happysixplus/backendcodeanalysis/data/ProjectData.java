94d3da5ea5986729de417af9c17f33498eff2337
何泊谋 <171250634@smail.nju.edu.cn>
2020-04-05 23:03:21 +0800

    style: Fix wanring

--- src/main/java/local/happysixplus/backendcodeanalysis/data/ProjectData.java
+++ src/main/java/local/happysixplus/backendcodeanalysis/data/ProjectData.java
@@ -1,11 +1,6 @@
 package local.happysixplus.backendcodeanalysis.data;
 
-import java.util.Optional;
-
-import javax.persistence.LockModeType;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 
 import local.happysixplus.backendcodeanalysis.po.ProjectPo;
 
