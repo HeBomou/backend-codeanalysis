10b6293e8d933802c322274bb36f901475c6a171
forsakenspirit <2369351080@qq.com>
2020-04-05 23:02:40 +0800

    fix(data)

--- src/main/java/local/happysixplus/backendcodeanalysis/data/ProjectData.java
+++ src/main/java/local/happysixplus/backendcodeanalysis/data/ProjectData.java
@@ -10,7 +10,4 @@ import org.springframework.data.jpa.repository.Lock;
 import local.happysixplus.backendcodeanalysis.po.ProjectPo;
 
 public interface ProjectData extends JpaRepository<ProjectPo, Long> {
-    @Override
-    @Lock(LockModeType.PESSIMISTIC_READ)
-    Optional<ProjectPo> findById(Long id);
 }
