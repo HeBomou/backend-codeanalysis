ae2b07d80dd01530e75bb36a63b1c2944e1ca854
何泊谋 <171250634@smail.nju.edu.cn>
2020-04-07 03:57:51 +0800

    style: fix

--- src/test/java/local/happysixplus/backendcodeanalysis/data/SubgraphDynamicDataTest.java
+++ src/test/java/local/happysixplus/backendcodeanalysis/data/SubgraphDynamicDataTest.java
@@ -33,56 +33,59 @@ class SubgraphDynamicDataTest {
     }
 
     @Test
-    public void testInsert(){
-        po=data.save(po);
-        SubgraphDynamicPo resPo=data.findById(po.getId()).get();
-        assertEquals(resPo.getId(),po.getId());
-        assertEquals(resPo.getProjectId(),po.getProjectId());
-        assertEquals(resPo.getName(),po.getName());
-    }
-    @Test
-    public void testUpdate(){
-        po=data.save(po);
-        po=new SubgraphDynamicPo(po.getId(),4396L,"Mother fucker"); 
-        po=data.save(po);
-        SubgraphDynamicPo resPo=data.findById(po.getId()).get();
-        assertEquals(resPo.getId(),po.getId());
-        assertEquals(resPo.getProjectId(),po.getProjectId());
-        assertEquals(resPo.getName(),po.getName());
-    }
-    @Test
-    public void testRemove(){
-        po=data.save(po);
-        data.deleteById(po.getId());
-        var res=data.findById(po.getId()).orElse(null);
-        assertEquals(res, null);
-    }
-    @Test
-    public void testFindByProjectId(){
-        po=data.save(po);
-        SubgraphDynamicPo po1=data.save(new SubgraphDynamicPo(765L,1L,"SKTKhan"));
-        SubgraphDynamicPo po2=data.save(new SubgraphDynamicPo(768L,1L,"SKTCLid"));
-        List<SubgraphDynamicPo> list=data.findByProjectId(1L);
-        list.sort((a,b)->(int)(a.getId()-b.getId()));
-        assertEquals(list.get(0).getId(),po.getId());
-        assertEquals(list.get(0).getProjectId(),po.getProjectId());
-        assertEquals(list.get(0).getName(),po.getName());
-        assertEquals(list.get(1).getId(),po1.getId());
-        assertEquals(list.get(1).getProjectId(),po1.getProjectId());
-        assertEquals(list.get(1).getName(),po1.getName());
-        assertEquals(list.get(2).getId(),po2.getId());
-        assertEquals(list.get(2).getProjectId(),po2.getProjectId());
-        assertEquals(list.get(2).getName(),po2.getName());
+    public void testInsert() {
+        po = data.save(po);
+        SubgraphDynamicPo resPo = data.findById(po.getId()).get();
+        assertEquals(resPo.getId(), po.getId());
+        assertEquals(resPo.getProjectId(), po.getProjectId());
+        assertEquals(resPo.getName(), po.getName());
     }
 
     @Test
-    public void testDeleteByProjectId(){
-        po=data.save(po);
-        data.save(new SubgraphDynamicPo(765L,1L,"SKTKhan"));
-        data.save(new SubgraphDynamicPo(768L,1L,"SKTCLid"));
-        data.deleteByProjectId(1L);
-        List<SubgraphDynamicPo> list=data.findByProjectId(1L);
-        assertEquals(list,new ArrayList<SubgraphDynamicPo>());
+    public void testUpdate() {
+        po = data.save(po);
+        po = new SubgraphDynamicPo(po.getId(), 4396L, "Mother fucker");
+        po = data.save(po);
+        SubgraphDynamicPo resPo = data.findById(po.getId()).get();
+        assertEquals(resPo.getId(), po.getId());
+        assertEquals(resPo.getProjectId(), po.getProjectId());
+        assertEquals(resPo.getName(), po.getName());
     }
-    
+
+    @Test
+    public void testRemove() {
+        po = data.save(po);
+        data.deleteById(po.getId());
+        var res = data.findById(po.getId()).orElse(null);
+        assertEquals(res, null);
+    }
+
+    @Test
+    public void testFindByProjectId() {
+        po = data.save(po);
+        SubgraphDynamicPo po1 = data.save(new SubgraphDynamicPo(765L, 1L, "SKTKhan"));
+        SubgraphDynamicPo po2 = data.save(new SubgraphDynamicPo(768L, 1L, "SKTCLid"));
+        List<SubgraphDynamicPo> list = data.findByProjectId(1L);
+        list.sort((a, b) -> (int) (a.getId() - b.getId()));
+        assertEquals(list.get(0).getId(), po.getId());
+        assertEquals(list.get(0).getProjectId(), po.getProjectId());
+        assertEquals(list.get(0).getName(), po.getName());
+        assertEquals(list.get(1).getId(), po1.getId());
+        assertEquals(list.get(1).getProjectId(), po1.getProjectId());
+        assertEquals(list.get(1).getName(), po1.getName());
+        assertEquals(list.get(2).getId(), po2.getId());
+        assertEquals(list.get(2).getProjectId(), po2.getProjectId());
+        assertEquals(list.get(2).getName(), po2.getName());
+    }
+
+    @Test
+    public void testDeleteByProjectId() {
+        po = data.save(po);
+        data.save(new SubgraphDynamicPo(765L, 1L, "SKTKhan"));
+        data.save(new SubgraphDynamicPo(768L, 1L, "SKTCLid"));
+        data.deleteByProjectId(1L);
+        List<SubgraphDynamicPo> list = data.findByProjectId(1L);
+        assertEquals(list, new ArrayList<SubgraphDynamicPo>());
+    }
+
 }
