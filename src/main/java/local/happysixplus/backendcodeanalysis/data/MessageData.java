522580080902c3dfdedae5037ff05b56626e26bc
胡丹丹 <171250034@smail.nju.edu.cn>
2020-05-21 16:48:21 +0800

    feat(impl):add new function about message

--- src/main/java/local/happysixplus/backendcodeanalysis/data/MessageData.java
+++ src/main/java/local/happysixplus/backendcodeanalysis/data/MessageData.java
@@ -1,9 +1,13 @@
 package local.happysixplus.backendcodeanalysis.data;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import local.happysixplus.backendcodeanalysis.po.MessagePo;
 
 public interface MessageData extends JpaRepository<MessagePo, Long> {
+
+    List<MessagePo> findBySenderIdAndReceiverId(Long senderId, Long receiverId);
 
 }
--- src/main/java/local/happysixplus/backendcodeanalysis/service/impl/MessageServiceImpl.java
+++ src/main/java/local/happysixplus/backendcodeanalysis/service/impl/MessageServiceImpl.java
@@ -1,13 +1,16 @@
 package local.happysixplus.backendcodeanalysis.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import local.happysixplus.backendcodeanalysis.data.MessageData;
+import local.happysixplus.backendcodeanalysis.po.MessagePo;
 import local.happysixplus.backendcodeanalysis.service.MessageService;
 import local.happysixplus.backendcodeanalysis.vo.MessageVo;
+import lombok.var;
 
 @Service
 public class MessageServiceImpl implements MessageService {
@@ -17,22 +20,29 @@ public class MessageServiceImpl implements MessageService {
 
     @Override
     public void addMessage(MessageVo vo) {
-
+        messageData.save(new MessagePo(null, vo.getSenderId(), vo.getReceiverId(), vo.getContent(), vo.getTime(),
+                vo.getIsRead()));
     }
 
     @Override
     public void removeMessage(Long id) {
-
+        messageData.deleteById(id);
     }
 
     @Override
     public void updateMessage(MessageVo vo) {
-
+        messageData.save(new MessagePo(vo.getId(), vo.getSenderId(), vo.getReceiverId(), vo.getContent(), vo.getTime(),
+                vo.getIsRead()));
     }
 
     @Override
     public List<MessageVo> getMessage(Long senderId, Long receiverId) {
-        return null;
+        var pos = messageData.findBySenderIdAndReceiverId(senderId, receiverId);
+        var res = new ArrayList<MessageVo>(pos.size());
+        for (var po : pos)
+            res.add(new MessageVo(po.getId(), po.getSenderId(), po.getReceiverId(), po.getContent(), po.getTime(),
+                    po.getIsRead()));
+        return res;
     }
 
 }
