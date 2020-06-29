638093ff17bca94e6288b1d880315a19909f6906
何泊谋 <171250634@smail.nju.edu.cn>
2020-06-30 00:09:28 +0800

    fix(controller): Fix contact controller

--- src/main/java/local/happysixplus/backendcodeanalysis/controller/ContactController.java
+++ src/main/java/local/happysixplus/backendcodeanalysis/controller/ContactController.java
@@ -1,16 +1,16 @@
 package local.happysixplus.backendcodeanalysis.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
 import local.happysixplus.backendcodeanalysis.service.MessageService;
 import local.happysixplus.backendcodeanalysis.vo.UserVo;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping(value = "/contact")
