05084ce2ccabc86a829edb62fb7b47a2fe05f2e7
forsakenspirit <2369351080@qq.com>
2020-07-18 17:23:12 +0800

    feat(controller): added contact controller interfaces

--- src/main/java/local/happysixplus/backendcodeanalysis/controller/ContactController.java
+++ src/main/java/local/happysixplus/backendcodeanalysis/controller/ContactController.java
@@ -8,9 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping(value = "/contact")
@@ -27,6 +32,31 @@ public class ContactController {
     @GetMapping(value="/{userId}/new")
     public Boolean getContactNew(@PathVariable Long userId) {
         return service.getContactNew(userId);
+    }
+    
+    @GetMapping(value="/{userId}/{contactUserId}")
+    public ContactVo getContact(@PathVariable Long userId,@PathVariable Long contactUserId){
+        return service.getContact(userId, contactUserId);
+    }
+
+    @PutMapping(value="/{userId}/{contactUserId}")
+    public void putContactRead(@PathVariable Long userId,@PathVariable Long contactUserId, @RequestParam int read){
+        service.updateContactRead(userId, contactUserId, read);
+    }
+
+    @GetMapping(value="/exist/{userId}/{contactUserId}")
+    public Boolean getExistsContact(@PathVariable Long userId,@PathVariable Long contactUserId) {
+        return service.existContact(userId, contactUserId);
+    }
+
+    @PostMapping(value="/{userId}/{contactUserId}")
+    public void addContact(@PathVariable Long userId,@PathVariable Long contactUserId,@RequestParam int read){
+        service.addContact(userId, contactUserId, read);
+    }
+
+    @DeleteMapping(value="/{userId}/{contactUserId}")
+    public void removceContact(@PathVariable Long userId,@PathVariable Long contactUserId){
+        service.removeContact(userId, contactUserId);
     }
     
 
